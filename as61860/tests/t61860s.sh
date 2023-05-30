c51b824943eb396bc54254fcafed7696928325b4
foldedtoad <foldedtoad@gmail.com>
2023-05-29 20:08:53 -0700

    more test updates

--- as61860/tests/t61860s.sh
+++ as61860/tests/t61860s.sh
@@ -4,3 +4,5 @@
 ../../asxscn/build/asxscn -x t61860s.lst
 ../../aslink/build/aslink -nxmu -g addr=0 -g reg=0 -g extrn=0 t61860s
 ../../asxscn/build/asxscn -xi t61860s.rst
+../../ihex2wav/build/ihex2wav -old -o wavefile.wav t61860s.rel
+audacity ./wavefile.wav &
